7ebeb6cb40848e1be6b9f0c5829780ca15551898
Peter <79867740+CodeMashine@users.noreply.github.com>
2023-05-06 11:41:10 +0300

    feat: add solution 5 task

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,34 @@
+function cssPicker(from, to, name) {
+  const fs = require("fs");
+  const path = require("path");
+
+  const pickFrom = path.resolve(__dirname, from);
+  const pickTo = path.resolve(__dirname, to);
+
+  fs.readdir(pickTo, (err, data) => {
+    if (err) console.log(err.message);
+    if (data.includes(name)) {
+      fs.unlink(path.resolve(pickTo, name), (err) => {
+        if (err) console.log(err.message);
+      });
+      fs.writeFile(path.resolve(pickTo, name), "", (err) => {
+        if (err) console.log(name + " created");
+      });
+    }
+  });
+
+  fs.readdir(pickFrom, (err, data) => {
+    if (err) console.log(err.message);
+    data.forEach((file) => {
+      if (!file.includes(".css")) return;
+      fs.readFile(path.resolve(pickFrom, file), "", (err, data) => {
+        if (err) console.log(err.message);
+        fs.appendFile(path.resolve(pickTo, name), data, (err) => {
+          if (err) console.log(err.message);
+        });
+      });
+    });
+  });
+}
+
+cssPicker("styles", "project-dist", "bundle.css");
