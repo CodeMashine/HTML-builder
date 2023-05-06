e6e580c516dfcce6aa35f9169462065f69ada243
Peter <79867740+CodeMashine@users.noreply.github.com>
2023-05-06 09:30:15 +0300

    feat : add solution 5 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,41 @@
+function copyDir(mainWay, copyFolder, nameNewFolder) {
+  const fs = require("fs");
+  const path = require("path");
 
+  fs.opendir(path.resolve(mainWay, nameNewFolder), (err) => {
+    if (err) {
+      fs.mkdir(path.resolve(mainWay, nameNewFolder), (err) => {
+        if (err) console.log(err.message);
+        console.log(nameNewFolder + " created");
+      });
+    } else {
+      fs.rmdir(path.resolve(mainWay, nameNewFolder), (err) => {
+        if (err) {
+          console.log(err.message);
+        } else {
+          console.log(nameNewFolder + " deleted");
+          copyDir(mainWay, copyFolder, nameNewFolder);
+        }
+      });
+    }
+  });
+
+  fs.readdir(path.resolve(mainWay, copyFolder), (err, files) => {
+    if (err) console.log(err.message);
+    if (files) {
+      const copyFrom = path.resolve(mainWay, copyFolder);
+      const copyTo = path.resolve(mainWay, nameNewFolder);
+      files.forEach((file) => {
+        fs.copyFile(
+          path.resolve(copyFrom, file),
+          path.resolve(copyTo, file),
+          (err) => {
+            if (err) console.log(console.log(message));
+          }
+        );
+      });
+    }
+  });
+}
+
+copyDir(__dirname, "files", "files-copy");
