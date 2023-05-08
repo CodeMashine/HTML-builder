41e03079e809a1e88e4092d3d455baa088a1a31e
Peter <79867740+CodeMashine@users.noreply.github.com>
2023-05-08 11:10:11 +0300

    feat:add 2 task solution

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -9,6 +9,7 @@ fs.writeFile(path.join(__dirname, "", "input.txt"), "", (err) => {
 stdout.write("hello! input somethink..\n");
 
 stdin.on("data", (data) => {
+  console.log("data - " , data) ;
   fs.appendFile(
     path.join(__dirname, "", "input.txt"),
     data.toString().trim() + " ",
@@ -16,13 +17,24 @@ stdin.on("data", (data) => {
       if (err) console.log(err);
     }
   );
+
 });
+
+
+process.on("SIGINT" , ()=>{
+  process.exit() ;
+}) ;
+
+process.on("exit" , ()=>{
+  stdout.write("\ngood luck\n") ;
+});
+
 
 process.on("exit", (code) => {
   if (code === 0) {
     // console.log("\nhave a nice day\n") ;
-    stdout.write(`\ngood luck\n`);
+    stdout.write("\ngood luck\n");
   } else {
-    console.log("\n something was broken\n");
+    console.log("\nsomething was broken\n");
   }
 });
